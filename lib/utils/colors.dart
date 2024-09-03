35e38e253f2ae87ea1e735740220950e4a09672c
Usman Abubakar <104586608+Usmandankama@users.noreply.github.com>
2024-09-03 22:50:45 +0100

    Update colors.dart

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -4,6 +4,6 @@ class AppColors {
   static const Color primaryColor = Color(0xFF32936F);
   static const Color appBlack = Colors.black;
   static const Color appWhite = Colors.white;
-
   static const Color fontGrey = Color(0xFF707070);
+  static const Color lightGrey = Color.fromARGB(255, 217, 217, 217);
 }
