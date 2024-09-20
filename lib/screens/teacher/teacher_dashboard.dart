58fd857239642442a45bbd7cc826c4e87f576ca2
Usman Abubakar <104586608+Usmandankama@users.noreply.github.com>
2024-09-20 14:51:04 +0100

    Update teacher_dashboard.dart

--- lib/screens/teacher/teacher_dashboard.dart
+++ lib/screens/teacher/teacher_dashboard.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:padi_learn/screens/teacher/components/analytics_section.dart';
 import 'package:padi_learn/utils/colors.dart';
+
+import 'components/courseList.dart';
+import 'components/profile_section.dart';
 
 class TeacherDashboardScreen extends StatefulWidget {
   const TeacherDashboardScreen({super.key});
@@ -10,9 +16,100 @@ class TeacherDashboardScreen extends StatefulWidget {
 }
 
 class _TeacherDashboardScreenState extends State<TeacherDashboardScreen> {
-  final String teacherName = "John Doe"; // Replace with actual teacher's name
-  final String profileImageUrl =
-      "https://via.placeholder.com/150"; // Replace with actual profile image URL
+  late Future<List<QueryDocumentSnapshot>> _userCourses;
+  String teacherName = "Loading...";
+  String profileImageUrl = "https://via.placeholder.com/150"; // Default profile image
+  int totalCoursesUploaded = 0;
+  double totalEarnings = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchTeacherInfo(); // Fetch teacher's info on init
+    _userCourses = _fetchUserCourses(); // Fetch user courses
+    _fetchTeacherEarningsAndCourses(); // Fetch earnings and course count
+  }
+
+  Future<void> _fetchTeacherInfo() async {
+    try {
+      final userId = FirebaseAuth.instance.currentUser?.uid;
+
+      if (userId == null) {
+        setState(() {
+          teacherName = 'User not logged in';
+        });
+        return;
+      }
+
+      final userDoc = await FirebaseFirestore.instance.collection('users').doc(userId).get();
+
+      if (userDoc.exists) {
+        final data = userDoc.data();
+        
+        if (data != null && data.containsKey('name')) {
+          setState(() {
+            teacherName = data['name'] ?? 'Unknown Name';
+            profileImageUrl = data['profileImageUrl'] ?? profileImageUrl;
+          });
+        } else {
+          setState(() {
+            teacherName = 'Name field missing';
+          });
+        }
+      } else {
+        setState(() {
+          teacherName = 'User document not found';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        teacherName = 'Error loading name';
+      });
+      print('Error fetching teacher info: $e');
+    }
+  }
+
+  Future<void> _fetchTeacherEarningsAndCourses() async {
+    try {
+      final userId = FirebaseAuth.instance.currentUser?.uid;
+
+      if (userId == null) return;
+
+      // Fetch teacher's courses
+      final querySnapshot = await FirebaseFirestore.instance
+          .collection('courses')
+          .where('userId', isEqualTo: userId)
+          .get();
+
+      int coursesCount = querySnapshot.docs.length;
+      double earnings = 0.0;
+
+      for (var courseDoc in querySnapshot.docs) {
+        final courseData = courseDoc.data();
+        if (courseData.containsKey('price') && courseData.containsKey('enrollments')) {
+          final double price = courseData['price'] ?? 0.0;
+          final int enrollments = courseData['enrollments'] ?? 0;
+          earnings += price * enrollments;
+        }
+      }
+
+      setState(() {
+        totalCoursesUploaded = coursesCount;
+        totalEarnings = earnings;
+      });
+    } catch (e) {
+      print('Error fetching teacher earnings and courses: $e');
+    }
+  }
+
+  Future<List<QueryDocumentSnapshot>> _fetchUserCourses() async {
+    final userId = FirebaseAuth.instance.currentUser!.uid;
+    final querySnapshot = await FirebaseFirestore.instance
+        .collection('courses')
+        .where('userId', isEqualTo: userId)
+        .get();
+    return querySnapshot.docs;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,15 +126,68 @@ class _TeacherDashboardScreenState extends State<TeacherDashboardScreen> {
         ),
         backgroundColor: AppColors.appWhite,
         elevation: 0,
-        iconTheme: IconThemeData(color: AppColors.primaryColor),
+        iconTheme: const IconThemeData(color: AppColors.primaryColor),
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.symmetric(horizontal: 16.0.w, vertical: 16.0.h),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            _buildProfileSection(),
+            ProfileSection(
+              teacherName: teacherName,
+              profileImageUrl: profileImageUrl,
+            ),
             SizedBox(height: 20.h),
+            
+            // EARNINGS SECTION
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Total Earnings',
+                  style: TextStyle(
+                    fontSize: 20.sp,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.primaryColor,
+                  ),
+                ),
+                Text(
+                  '\$${totalEarnings.toStringAsFixed(2)}',
+                  style: TextStyle(
+                    fontSize: 20.sp,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.primaryColor,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20.h),
+
+            // COURSES UPLOADED SECTION
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Courses Uploaded',
+                  style: TextStyle(
+                    fontSize: 20.sp,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.primaryColor,
+                  ),
+                ),
+                Text(
+                  '$totalCoursesUploaded',
+                  style: TextStyle(
+                    fontSize: 20.sp,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.primaryColor,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20.h),
+
+            // COURSE ANALYTICS SECTION
             Text(
               'Course Analytics',
               style: TextStyle(
@@ -47,8 +197,10 @@ class _TeacherDashboardScreenState extends State<TeacherDashboardScreen> {
               ),
             ),
             SizedBox(height: 10.h),
-            _buildAnalyticsSection(),
+            const AnalyticsSection(),
             SizedBox(height: 20.h),
+
+            // YOUR COURSES SECTION
             Text(
               'Your Courses',
               style: TextStyle(
@@ -58,170 +210,23 @@ class _TeacherDashboardScreenState extends State<TeacherDashboardScreen> {
               ),
             ),
             SizedBox(height: 10.h),
-            _buildYourCoursesList(),
+            FutureBuilder<List<QueryDocumentSnapshot>>(
+              future: _userCourses,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                if (snapshot.hasError) {
+                  return const Center(child: Text('Error loading courses'));
+                }
+                if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return const Center(child: Text('No courses found'));
+                }
+                return CoursesList(courses: snapshot.data!);
+              },
+            ),
             SizedBox(height: 20.h),
-            _buildUploadCourseButton(context),
           ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildProfileSection() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: <Widget>[
-        CircleAvatar(
-          radius: 30.r,
-          backgroundImage: NetworkImage(profileImageUrl),
-        ),
-        SizedBox(width: 15.w),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              teacherName,
-              style: TextStyle(
-                fontSize: 20.sp,
-                fontWeight: FontWeight.bold,
-                color: AppColors.primaryColor,
-              ),
-            ),
-            Text(
-              'Teacher',
-              style: TextStyle(
-                fontSize: 16.sp,
-                color: AppColors.primaryColor.withOpacity(0.7),
-              ),
-            ),
-          ],
-        ),
-      ],
-    );
-  }
-
-  Widget _buildAnalyticsSection() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        _buildAnalyticsItem('Total Earnings', '\$5000', Icons.monetization_on),
-        SizedBox(width: 10.w),
-        _buildAnalyticsItem('Courses Sold', '200', Icons.school),
-        SizedBox(width: 10.w),
-        _buildAnalyticsItem('Avg. Rating', '4.5/5', Icons.star),
-      ],
-    );
-  }
-
-  Widget _buildAnalyticsItem(String title, String value, IconData icon) {
-    return Expanded(
-      child: Container(
-        padding: EdgeInsets.all(16.0.w),
-        decoration: BoxDecoration(
-          color: AppColors.primaryColor.withOpacity(0.1),
-          borderRadius: BorderRadius.circular(15.r),
-        ),
-        child: Column(
-          children: <Widget>[
-            Icon(icon, color: AppColors.primaryColor, size: 30.sp),
-            SizedBox(height: 10.h),
-            Text(
-              title,
-              style: TextStyle(
-                fontSize: 14.sp,
-                color: AppColors.primaryColor,
-              ),
-            ),
-            SizedBox(height: 5.h),
-            Text(
-              value,
-              style: TextStyle(
-                fontSize: 16.sp,
-                fontWeight: FontWeight.bold,
-                color: AppColors.primaryColor,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildYourCoursesList() {
-    return ListView.builder(
-      physics: NeverScrollableScrollPhysics(), // Prevent scrolling
-      shrinkWrap: true,
-      itemCount: 5, // Replace with your dynamic item count
-      itemBuilder: (context, index) {
-        return Container(
-          margin: EdgeInsets.only(bottom: 16.h),
-          padding: EdgeInsets.all(16.w),
-          decoration: BoxDecoration(
-            color: AppColors.primaryColor.withOpacity(0.1),
-            borderRadius: BorderRadius.circular(10.r),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    'Course Title',
-                    style: TextStyle(
-                      color: AppColors.primaryColor,
-                      fontSize: 16.sp,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 5.h),
-                  Text(
-                    'Students Enrolled: 50', // Placeholder for student count
-                    style: TextStyle(
-                      color: AppColors.primaryColor,
-                      fontSize: 14.sp,
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  IconButton(
-                    icon: Icon(Icons.edit, color: AppColors.primaryColor),
-                    onPressed: () {
-                      // Handle course edit
-                    },
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.delete, color: AppColors.primaryColor),
-                    onPressed: () {
-                      // Handle course delete
-                    },
-                  ),
-                ],
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-
-  Widget _buildUploadCourseButton(BuildContext context) {
-    return ElevatedButton.icon(
-      onPressed: () {
-        // Navigate to course upload screen
-      },
-      style: ElevatedButton.styleFrom(
-        backgroundColor: AppColors.primaryColor,
-        padding: EdgeInsets.symmetric(horizontal: 60.0.w, vertical: 20.0.h),
-      ),
-      icon: Icon(Icons.upload_file, color: AppColors.appWhite),
-      label: Text(
-        'Upload Course',
-        style: TextStyle(
-          fontSize: 18.sp,
-          color: AppColors.appWhite,
         ),
       ),
     );
