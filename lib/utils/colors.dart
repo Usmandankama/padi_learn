ddb20de29cec7832c758f8958b5c5d0069536d14
Usman Abubakar <104586608+Usmandankama@users.noreply.github.com>
2024-09-24 01:09:49 +0100

    Update colors.dart

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primaryColor = Color(0xFF32936F);
+  static const Color primaryAccent = Color(0x1832936F);
   static const Color appBlack = Colors.black;
+  static const Color richBlack = Color(0xFF0D1B2A);
   static const Color appWhite = Colors.white;
   static const Color fontGrey = Color(0xFF707070);
+  static const Color beige = Color(0xFF253031);
   static const Color lightGrey = Color.fromARGB(255, 217, 217, 217);
 }
