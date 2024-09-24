1f626ddd2f646bc5482d11b4c6298a662a75e4bb
Usman Abubakar <104586608+Usmandankama@users.noreply.github.com>
2024-09-24 01:10:11 +0100

    Update courseGrid.dart

--- lib/screens/marketplace/components/courseGrid.dart
+++ lib/screens/marketplace/components/courseGrid.dart
@@ -1,28 +1,68 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-import 'courseCard.dart';
 
 class CoursesGrid extends StatelessWidget {
-  final List<QueryDocumentSnapshot> courses;
+  final List courses;
 
-  const CoursesGrid({super.key, required this.courses});
+  const CoursesGrid({Key? key, required this.courses}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
+      padding: const EdgeInsets.all(8.0),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,  // Number of courses per row
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        childAspectRatio: 0.75,
+        crossAxisCount: 2,
+        childAspectRatio: 1.0,
+        crossAxisSpacing: 10.0,
+        mainAxisSpacing: 10.0,
       ),
-      padding: const EdgeInsets.all(10),
       itemCount: courses.length,
       itemBuilder: (context, index) {
         final courseData = courses[index].data() as Map<String, dynamic>;
+        final title = courseData['title'];
+        final thumbnailUrl = courseData['thumbnailUrl']; // Fetch thumbnail URL
 
-        return CourseCard(courseData: courseData);
+        return Card(
+          elevation: 3,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(15.0),
+            child: Stack(
+              fit: StackFit.expand,
+              children: <Widget>[
+                // Display the custom thumbnail as the background
+                Image.network(
+                  thumbnailUrl,
+                  fit: BoxFit.cover,
+                ),
+                // Overlay course title
+                Positioned(
+                  bottom: 10,
+                  left: 10,
+                  right: 10,
+                  child: Text(
+                    title,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      shadows: [
+                        Shadow(
+                          blurRadius: 10.0,
+                          color: Colors.black,
+                          offset: Offset(0.0, 0.0),
+                        ),
+                      ],
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
       },
     );
   }
