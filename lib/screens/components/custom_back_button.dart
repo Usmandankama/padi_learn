36f6691e422a33bb0cacd785499badc8b8519489
Usman Abubakar <104586608+Usmandankama@users.noreply.github.com>
2024-10-17 00:10:10 +0100

    Update custom_back_button.dart

--- lib/screens/components/custom_back_button.dart
+++ lib/screens/components/custom_back_button.dart
@@ -7,20 +7,20 @@
 
 //   @override
 //   Widget build(BuildContext context) {
-//     return Container(
-//       height: 35.h,
-//       width: 80.w,
-//       decoration: BoxDecoration(
-//         color: AppColors.appWhite,
-//         boxShadow: [
-//           BoxShadow(
-//             blurRadius: 2,
-//             color: Colors.black.withOpacity(.4),
-//             spreadRadius: 2,
-//           ),
-//         ],
-//         borderRadius: BorderRadius.circular(30.r),
-//       ),
+    // return Container(
+    //   height: 35.h,
+    //   width: 80.w,
+    //   decoration: BoxDecoration(
+    //     color: AppColors.appWhite,
+    //     boxShadow: [
+    //       BoxShadow(
+    //         blurRadius: 2,
+    //         color: Colors.black.withOpacity(.4),
+    //         spreadRadius: 2,
+    //       ),
+    //     ],
+    //     borderRadius: BorderRadius.circular(30.r),
+    //   ),
 //       child: Row(
 //         mainAxisAlignment: MainAxisAlignment.center,
 //         children: [
