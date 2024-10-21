7d16d2a7b022f05fe43d12f59e40e44c4430e93e
Usman Abubakar <104586608+Usmandankama@users.noreply.github.com>
2024-10-21 21:56:14 +0100

    Updated the scroll on the market screen

--- lib/screens/marketplace/marketplace_screen.dart
+++ lib/screens/marketplace/marketplace_screen.dart
@@ -37,93 +37,90 @@ class _MarketplaceScreenState extends State<MarketplaceScreen> {
         ),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            // Search Field
-            Padding(
-              padding: const EdgeInsets.all(15),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Search courses',
-                  prefixIcon: const Icon(
-                    Icons.search,
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Search courses',
+                prefixIcon: const Icon(
+                  Icons.search,
+                  color: AppColors.primaryColor,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: BorderSide(
                     color: AppColors.primaryColor,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                    borderSide: BorderSide(
-                      color: AppColors.primaryColor,
-                      width: 2.w,
-                    ),
+                    width: 2.w,
                   ),
                 ),
-                onChanged: (value) {
-                  setState(() {
-                    controller.searchQuery.value = value; // Update search query
-                  });
-                },
               ),
+              onChanged: (value) {
+                setState(() {
+                  controller.searchQuery.value = value; // Update search query
+                });
+              },
             ),
-            // Horizontal ListView for category selection
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: SizedBox(
-                height: 50.h, // Adjust the height as necessary
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: categories.length,
-                  itemBuilder: (context, index) {
-                    String category = categories[index];
-                    bool isSelected = controller.selectedFilter.value == category;
-
-                    return GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          controller.selectedFilter.value = category; // Update selected category
-                        });
-                      },
-                      child: Container(
-                        margin: EdgeInsets.symmetric(horizontal: 5.0),
-                        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                        decoration: BoxDecoration(
-                          color: isSelected ? AppColors.primaryColor : Colors.grey[300],
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Center(
-                          child: Text(
-                            category,
-                            style: TextStyle(
-                              color: isSelected ? Colors.white : Colors.black,
-                            ),
+          ),
+          // Horizontal ListView for category selection
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: SizedBox(
+              height: 50.h, // Adjust the height as necessary
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: categories.length,
+                itemBuilder: (context, index) {
+                  String category = categories[index];
+                  bool isSelected = controller.selectedFilter.value == category;
+                    
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        controller.selectedFilter.value = category; // Update selected category
+                      });
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                      decoration: BoxDecoration(
+                        color: isSelected ? AppColors.primaryColor : Colors.grey[300],
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Center(
+                        child: Text(
+                          category,
+                          style: TextStyle(
+                            color: isSelected ? Colors.white : Colors.black,
                           ),
                         ),
                       ),
-                    );
-                  },
-                ),
+                    ),
+                  );
+                },
               ),
             ),
-            // Display the courses in a GridView
-            SizedBox(
-              height: 500.h,
-              child: controller.courses.isEmpty
-                  ? const Center(child: CircularProgressIndicator())
-                  : Builder(builder: (context) {
-                      // Apply search and filter
-                      final filteredCourses = controller.filterCourses();
-
-                      if (filteredCourses.isEmpty) {
-                        return const Center(child: Text('No courses available'));
-                      }
-
-                      return Padding(
-                        padding: const EdgeInsets.all(15),
-                        child: CoursesList(courses: filteredCourses));
-                    }),
-            ),
-          ],
-        ),
+          ),
+          // Display the courses in a GridView
+          SizedBox(
+            // height: 500.h,
+            child: controller.courses.isEmpty
+                ? const Center(child: CircularProgressIndicator())
+                : Builder(builder: (context) {
+                    // Apply search and filter
+                    final filteredCourses = controller.filterCourses();
+                    
+                    if (filteredCourses.isEmpty) {
+                      return const Center(child: Text('No courses available'));
+                    }
+                    
+                    return Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: CoursesList(courses: filteredCourses));
+                  }),
+          ),
+        ],
       ),
     );
   }
